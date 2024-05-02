ee187ce2ca463411080b2859774503c070b13208
giginet <giginet.net@gmail.com>
2024-05-02 17:39:48 +0900

    Implement package structure

--- Package.swift
+++ Package.swift
@@ -5,10 +5,53 @@ import PackageDescription
 
 let package = Package(
     name: "swift-testing-revolutionary",
+    platforms: [
+        .macOS(.v14),
+    ],
+    products: [
+        .executable(
+            name: "swift-testing-revolutionary",
+            targets: ["swift-testing-revolutionary"]
+        ),
+        .plugin(
+            name: "RevolutionaryPlugin",
+            targets: ["RevolutionaryPlugin"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-syntax.git",
+            .upToNextMajor(from: "510.0.1")
+        ),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RevolutionKit",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+            ]
+        ),
+        .plugin(
+            name: "RevolutionaryPlugin",
+            capability: .command(
+                intent: .custom(
+                    verb: "revolt",
+                    description: "Convert XCTest cases to swift-testing"
+                ),
+                permissions: [.writeToPackageDirectory(reason: "Rewrite implementations")]
+            )
+        ),
         .executableTarget(
-            name: "swift-testing-revolutionary"),
+            name: "swift-testing-revolutionary",
+            dependencies: [
+                .target(name: "RevolutionKit"),
+            ]
+        ),
+        .testTarget(
+            name: "RevolutionKitTests",
+            dependencies: [
+                .target(name: "RevolutionKit"),
+            ]
+        ),
     ]
 )
---
+++ Sources/RevolutionKit/Runner.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+package struct Runner {
+}
